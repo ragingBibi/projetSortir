072a8c09deda4b45fb473c5c9451a7569e3cf351
Valentin <valentin.pirot2023@campus-eni.fr>
2024-03-14 16:41:01 +0100

    Ajout élément partiel twig : _searchbar.html.twig MAJ du HomeController : intégration de la recherche par filtres MAJ de la vue twig home.html.twig

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,11 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\EventSearch;
 use App\Entity\User;
+use App\Form\EventSearchType;
 use App\Repository\EventRepository;
 use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -15,14 +18,20 @@ use Symfony\Component\Routing\Attribute\Route;
 class HomeController extends AbstractController
 {
     #[Route(path: '', name: 'home', methods: ['GET'])]
-    public function home(EventRepository $eventRepository): Response
+    public function home(EventRepository $eventRepository, Request $request): Response
     {
         //on récupère l'utilisateur connecté
         $user = $this->getUser();
+        //On crée le formulaire de recherche
+        $search = new EventSearch();
+        $form = $this->createForm(EventSearchType::class, $search);
+        $form->handleRequest($request);
 
         return $this->render('home/home.html.twig', [
             //on récupeère tous les events créés
             'events' => $eventRepository->findAll(),
+            //On passe le formulaire de recherche à la vue
+            'form' => $form->createView()
         ]);
     }
 }
