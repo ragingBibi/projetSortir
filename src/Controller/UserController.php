7a81db15d2d42458c4ff65d3f0baea717b5b16d3
sarahMoraines <sidmomo29@gmail.com>
2024-03-14 09:26:58 +0100

    PEtites modif de routes

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -55,7 +55,7 @@ class UserController extends AbstractController
         ]);
     }
 
-    #[Route('/profile/{id}', name: 'app_show_profile', methods: ['GET', 'POST'])]
+    #[Route('/profile/{id}', name: 'app_edit_profile', methods: ['GET', 'POST'])]
     public function updateUser(User $user, Request $request, EntityManagerInterface $em, SluggerInterface $slugger): Response
     {
         $form = $this->createForm(UserType::class, $user);
@@ -74,7 +74,7 @@ class UserController extends AbstractController
             $em->flush();
 
             $this->addFlash('success text-center', 'Votre profil a bien été mis à jour');
-            return $this->redirectToRoute('app_show_profile', ['id' => $user->getId()]);
+            return $this->redirectToRoute('app_edit_profile', ['id' => $user->getId()]);
         }
 
         return $this->render('user/edit.html.twig', [
@@ -82,24 +82,6 @@ class UserController extends AbstractController
             'user' => $user
         ]);
     }
-
-/*    #[Route('/{id}/edit', name: 'app_user_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, User $user, EntityManagerInterface $entityManager): Response
-    {
-        $form = $this->createForm(UserType::class, $user);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->flush();
-
-            return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->render('user/edit.html.twig', [
-            'user' => $user,
-            'form' => $form,
-        ]);
-    }*/
 
     #[Route('/{id}', name: 'app_user_delete', methods: ['POST'])]
     public function delete(Request $request, User $user, EntityManagerInterface $entityManager): Response
