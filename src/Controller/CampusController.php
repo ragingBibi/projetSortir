74f8c22b3e3de4e031332fc3836c7fd9f81cef9b
Paulina <paulina.ladyga2023@campus-eni.fr>
2024-03-14 16:07:03 +0100

    Gestion des villes (cities) + des sites (campus) + lieux (venues) pour Admin.

---
+++ src/Controller/CampusController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Campus;
+use App\Form\CampusType;
+use App\Repository\CampusRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route(path: '/campus', name: 'app_campus_')]
+#[IsGranted('ROLE_ADMIN')]
+class CampusController extends AbstractController
+{
+    #[Route('/', name: 'index', methods: ['GET'])]
+    public function index(CampusRepository $campusRepository): Response
+    {
+        return $this->render('campus/index.html.twig', [
+            'campuses' => $campusRepository->findAll(),
+        ]);
+    }
+
+
+    #[Route('/{id}/edit', name: 'edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Campus $campus, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(CampusType::class, $campus);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_campus_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('campus/edit.html.twig', [
+            'campus' => $campus,
+            'form' => $form,
+        ]);
+    }
+
+
+    #[Route('/new', name: 'new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $campus = new Campus();
+        $form = $this->createForm(CampusType::class, $campus);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($campus);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_campus_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('campus/new.html.twig', [
+            'campus' => $campus,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'show', methods: ['GET'])]
+    public function show(Campus $campus): Response
+    {
+        return $this->render('campus/show.html.twig', [
+            'campus' => $campus,
+        ]);
+    }
+
+
+
+    #[Route('/{id}', name: 'delete', methods: ['POST'])]
+    public function delete(Request $request, Campus $campus, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$campus->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($campus);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_campus_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
