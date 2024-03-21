35eb4936b3458d1891437c4630695de8a2bbf794
sarahMoraines <sidmomo29@gmail.com>
2024-03-21 10:21:16 +0100

    Changement traductions

--- src/Form/CityType.php
+++ src/Form/CityType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\City;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,8 +13,8 @@ class CityType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('zipCode')
+            ->add('name', TextType::class, ['label' => 'Nom'])
+            ->add('zipCode', TextType::class, ['label' => 'Code postal'])
         ;
     }
 
